bdccae3101076e09d3aa0db10f151c8e6148f0be
Martin Lansler <martin.lansler@gmail.com>
2010-12-16 00:07:57 +0100

    - tested schema validation from StAX source - added interface for an XML command element builder

---
+++ src/main/java/commandflow/builder/xml/XmlElementCommandBuilder.java
@@ -0,0 +1,35 @@
+/**
+ * Copyright 2010 Martin Lansler (elansma), Anders Jacobsson
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package commandflow.builder.xml;
+
+import java.util.Map;
+
+import commandflow.Command;
+
+/**
+ * Interface for a builder that can create a command instance from an XML element with associated attributes.
+ * @author elansma
+ * @param <C> the context class of the commands
+ */
+public interface XmlElementCommandBuilder<C> {
+    /**
+     * Called to create a new command instance
+     * @param elementName the name of the XML element
+     * @param attributes the map of attributes (maps from attribute name to value)
+     * @return a new command instance
+     */
+    Command<C> build(String elementName, Map<String, String> attributes);
+}
---
+++ src/test/java/tests/StAXSchemaValidator.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright 2010 Martin Lansler (elansma), Anders Jacobsson
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package tests;
+
+import java.io.InputStream;
+
+import javax.xml.XMLConstants;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.transform.stax.StAXSource;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import javax.xml.validation.Validator;
+
+import org.testng.annotations.Test;
+
+/**
+ * Tests to validate XML from a {@link StAXSource} via a schema {@link Validator}
+ * @author elansma
+ */
+public class StAXSchemaValidator {
+    @Test
+    public void testValidation() throws Exception {
+        InputStream is = getResource("commandflow/builder/xml/command.xml");
+        StAXSource commandXml = new StAXSource(XMLInputFactory.newInstance().createXMLStreamReader(is));
+        is = getResource("commandflow/builder/xml/command.xsd");
+        Schema schema = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI).newSchema(new StreamSource(is));
+        schema.newValidator().validate(commandXml);
+    }
+
+    private InputStream getResource(String resource) {
+        return getClass().getClassLoader().getResourceAsStream(resource);
+    }
+}
