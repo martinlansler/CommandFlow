a7205b37d20119392ff5e5b6a1bd98ea457586bc
Martin Lansler <martin.lansler@gmail.com>
2011-01-28 23:07:40 +0100

    changed from testng to junit

--- src/test/java/commandflow/test/command/ConditionCommand.java
+++ src/test/java/commandflow/test/command/ConditionCommand.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package commandflow.command;
+package commandflow.test.command;
 
 import java.util.concurrent.atomic.AtomicLong;
 
